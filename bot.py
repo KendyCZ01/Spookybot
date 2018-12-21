09162be28987177b67217251f276b3367c01ffa8
MissNela <44345612+MissNela@users.noreply.github.com>
2018-12-21 14:52:40 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ async def ghelp():
     
 @client.command()
 async def modhelp():
-    embed = discord.Embed(url = "https://cdn.discordapp.com/attachments/468928524267290634/525662927529836574/f054ab37k2ny.gif", title = "Pomoc Pro Moderátory!", 0x006400)
+    embed = discord.Embed(title = "Pomoc Pro Moderátory!", 0x006400)
     embed.add_field(name = "S!clear", value = "Smaže daný počet zpráv!",inline=False)
     embed.set_footer(text = "Pomoc přivolána {0}!".format(message.author.name))
     await client.say(embed=embed)
