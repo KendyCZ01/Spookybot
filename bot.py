09110a896d71f14dcd8184e1c61af386a180f52b
JustVojta <46031739+00Vojta00@users.noreply.github.com>
2018-12-21 17:23:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ async def clear(ctx, number):
        
     try:
         await client.delete_messages(mgs)          
-        await client.say(str(number)+' zpráv vymazáno <:sucess:525707476746633226>')
+        await client.say(str'<:sucess:525707476746633226>'(number)+' zpráv vymazáno')
      
     except discord.Forbidden:
         await client.say(embed=Forbidden)
