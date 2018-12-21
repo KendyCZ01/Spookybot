4033c55ba6c86c2bde23c78ca672548e243fd694
00Vojta00 <46031739+00Vojta00@users.noreply.github.com>
2018-12-21 14:32:59 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,16 +17,16 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 
 
-client = commands.Bot(command_prefix = '!', case_insensitive=False)
+client = commands.Bot(command_prefix = 's!', case_insensitive=False)
 
 client.remove_command('help')
 
 @client.event
 async def on_ready():
     channel = discord.utils.get(client.get_all_channels(), name='🌎・hlavní-chat')
-    await client.change_presence(game=discord.Game(name= "Zkuste s!help"))
+    await client.change_presence(game=discord.Game(name= "Prefix: s!"))
     print("The bot is online and connected with Discord!") 
-    await client.send_message(channel, "``Jsem tu a připraven!!``")
+    await client.send_message(channel, "`Jsem tu a připraven!!`")
     
 def owner(ctx):
     return ctx.message.author.id == "342364288310312970", "522048274689949712"
@@ -34,10 +34,10 @@ def owner(ctx):
 @client.command()
 async def help():
     embed = discord.Embed(title = "Pomoc!", color = 0x00FF00)
-    embed.add_field(name = "!ghelp", value = "Ukáže Pomoc pro všechny! (připravuje se)",inline = False)
-    embed.add_field(name = "!modhelp", value = "Ukáže ti pomoc pro moderátory! (připravuje se)",inline = False)
-    embed.add_field(name = "!helpfun", value = "Ukáže ti vtipné přikazy! (doděláváme)",inline = False)
-    embed.set_footer(text = "Bot vytvořen JustNela#6666")
+    embed.add_field(name = "s!ghelp", value = "Ukáže Pomoc pro všechny! (připravuje se)",inline = False)
+    embed.add_field(name = "s!modhelp", value = "Ukáže ti pomoc pro moderátory! (připravuje se)",inline = False)
+    embed.add_field(name = "s!helpfun", value = "Ukáže ti vtipné přikazy! (doděláváme)",inline = False)
+    embed.set_footer(text = "Bot vytvořen JustNela#6666 & JustVojta#6969")
     embed.set_image(url = "https://image.shutterstock.com/image-vector/help-bulb-word-cloud-collage-450w-415140307.jpg")
     await client.say(embed=embed)
     
