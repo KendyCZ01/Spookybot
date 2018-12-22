4061c6ba831c4cbd47062741edca13cc8825b758
MissNela <44345612+MissNela@users.noreply.github.com>
2018-12-22 11:00:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ async def ban(ctx,user:discord.Member):
         return		 
 @client.command()
 async def JustNela():
-    embed = discord.Embed(title = "Info o JustNela#6666", icon_url = "https://cdn.discordapp.com/attachments/468928524267290634/525662927529836574/f054ab37k2ny.gif", color = 0x663399)
+    embed = discord.Embed(title = "Info o JustNela#6666", color = 0x00FF00)
     embed.add_field(name = "Info", value = "",inline = False)
     embed.add_field(name = "Jmeno:", value = "@JustNela#6666",inline = False)
     embed.add_field(name = "Status", value = "Nejčastěji Online!",inline = False)
